aa17199365d509cf4358ab4001ac544f28168cee
GomesJ7 <98333692+GomesJ7@users.noreply.github.com>
2022-07-11 15:22:13 +0200

    readme

--- src/Sections/Projets/index.js
+++ src/Sections/Projets/index.js
@@ -1,7 +1,7 @@
 import React, { lazy } from "react";
 import styled from "styled-components";
 import wave from "../../assets/waves2.svg";
-import { projects } from "../../data";
+import { projects } from "./data";
 import Slider from "react-slick";
 import "../../../node_modules/slick-carousel/slick/slick.css";
 import "../../../node_modules/slick-carousel/slick/slick-theme.css";
